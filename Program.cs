72ef6c5d63c8e78c79078f3e246a3960d81b4d54
hanSomBorISkogen <joel-aannmo@hotmail.com>
2022-09-10 11:13:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPN_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

6ebccc3e806d73a6f80766857fbdf8227008b2bb
MarrisSparris <martin.no.nilsson@gmail.com>
2022-09-17 12:44:43 +0200

    Added Files folder

---
+++ Files/WriteToFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPN_Calculator.Files
+{
+    internal class WriteToFile
+    {
+    }
+}
--- Model/Token.cs
+++ Model/Token.cs
@@ -2,11 +2,23 @@
 {
     public class Token
     {
+        // Stack variabel??
         public static Stack<Token> tokens;
 
-        public Token(Stack<Token> tokens)
+        public Token()
         {
             Token.tokens = tokens;
+            //tokens.Push
         }
+
+        /*
+        1. Användaren skriver in en expression
+        2. Strängen hamnar i en variabel och splitas där ' ' påträffas
+        3. För varje tecken avgöra om det är ett giltigt token
+        4. Pusha token på stacken tills strängen är tom
+        5. Poppa tokens från stacken och mönstermatcha med giltiga operatorer
+        6. Gör beräkningarna och printa "Result"
+        7. Fortsätta skriva in expression eller terminera?         
+        */
     }
 }
